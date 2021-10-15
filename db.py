ddcc6dac4cdf03e856f2c9025bd26f639391dff8
Felipe Gomes Duarte <xfelipegd@gmail.com>
2021-10-15 01:43:45 -0300

    Post and comment models.

--- db.py
+++ db.py
@@ -1,14 +1,19 @@
 from flask_sqlalchemy import SQLAlchemy
 from marshmallow import ValidationError
+from sqlalchemy.orm import backref
 from schemas import UserSchema
 from werkzeug.security import generate_password_hash
+
 
 db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
-    email = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), nullable=False, unique=True)
     password = db.Column(db.String(255), nullable=False)
+
+    posts = db.relationship('Post', backref='user', lazy='dynamic')
+    comments = db.relationship('Comment', backref='user', lazy='dynamic')
 
     def __repr__(self) -> str:
         return self.email
@@ -22,3 +27,19 @@ class User(db.Model):
         )
         self.email = valid_data['email']
         self.password = generate_password_hash(valid_data['password'])
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    title = db.Column(db.Text(), nullable=False)
+    body = db.Column(db.Text(), nullable=False)
+    user_id = db.Column(db.Integer(), db.ForeignKey('user.id'), nullable=False)
+
+    comments = db.relationship('Comment', backref='post', lazy='dynamic')
+
+
+class Comment(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    body = db.Column(db.Text(), nullable=False)
+    user_id = db.Column(db.Integer(), db.ForeignKey('user.id'), nullable=False)
+    post_id = db.Column(db.Integer(), db.ForeignKey('post.id'), nullable=False)
